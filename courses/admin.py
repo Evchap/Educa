ffd575fa0a7acbfa60359046085dd97439ce2191
“Evgen <evchap8888@gmail.com>
2024-06-08 19:27:49 +0300

    Registration of models in the admin panel.

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Subject, Course, Module
+
+
+@admin.register(Subject) # page 4
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline): # page 4
+    model = Module
+
+
+@admin.register(Course) # page 4
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
+
