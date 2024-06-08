85ad808167a1d93bf1dd9b0698889d546a8d509d
“Evgen <evchap8888@gmail.com>
2024-06-08 18:03:58 +0300

    The "courses" application has been created. Course models have been created: Subject, Course, Module.

--- educa/urls.py
+++ educa/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Educa project.
+URL configuration for educa project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
