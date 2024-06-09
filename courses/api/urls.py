ddf02c4e7450808c7d42f6a144ad006a314616d9
“Evgen <evchap8888@gmail.com>
2024-06-09 12:02:20 +0300

    Added a set of views for the Course model.

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,11 +1,19 @@
 # from django.conf.urls import url
-from django.urls import re_path
+from django.urls import re_path, include
 from django.urls import re_path as url
 from . import views
+from rest_framework import routers
+
 
 app_name = 'courses'
+
+router = routers.DefaultRouter() #iss22
+router.register('courses', views.CourseViewSet) #iss22
+
 
 urlpatterns = [
     url(r'^subjects/$', views.SubjectListView.as_view(), name='subject_list'), # iss17
     url(r'^subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='subject_detail'),# iss17
+    url(r'^', include(router.urls)), #iss22
+
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 
@@ -12,3 +12,10 @@ class SubjectDetailView(generics.RetrieveAPIView): # iss17
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet): #iss22
+    queryset = Course.objects.all() #iss22
+    serializer_class = CourseSerializer #iss22
